3cde0c1620186795aa00d0f1ec856f345890c5c9
GitHub Action <action@github.com>
2024-08-06 15:06:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/jasontressler/intellijjaspersyntaxconverter/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jasontressler/intellijjaspersyntaxconverter/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jasontressler.intellijjaspersyntaxconverter.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
